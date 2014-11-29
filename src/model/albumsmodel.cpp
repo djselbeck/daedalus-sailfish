a6df40339a55a14f83570a5bfe16502bc882c01f
Hendrik Borghorst <hendrik@zakynthos.djselbeck.de>
2014-11-29 10:55:17 +0100

    New model structure

--- src/model/albumsmodel.cpp
+++ src/model/albumsmodel.cpp
@@ -5,20 +5,69 @@
 #include <QSparqlResult>
 
 AlbumsModel::AlbumsModel(QObject *parent, QSparqlConnection *connection) :
-    QSparqlQueryModel(parent)
+    QAbstractListModel(parent)
 {
     if ( connection != NULL ) {
         mConnection = connection;
         mAlbumsQueryString = "SELECT ?title COUNT(?piece) as ?trackcount SUM(?duration) as ?totalduration ?artistname WHERE { ?piece a nmm:MusicPiece; nfo:duration ?duration; nmm:musicAlbum ?titleObj . ?titleObj nmm:albumTitle ?title . ?titleObj nmm:albumArtist ?titleartistObj . ?titleartistObj nmm:artistName ?artistname } GROUP by ?title";
     }
-    connect(this,SIGNAL(finished()),this,SIGNAL(albumsReady()));
+    mSparqlModel = new QSparqlQueryModel(this);
+    connect(mSparqlModel,SIGNAL(finished()),this,SLOT(sparqlModelfinished()));
 }
 
 
 void AlbumsModel::requestAlbums()
 {
     // Initialize the query
-    clear();
+    mSparqlModel->clear();
     qDebug() << "getting albums";
-    setQuery(QSparqlQuery(mAlbumsQueryString),*mConnection);
+    // See qsparqlquerymodel.cpp and 4 from title, trackcount, totalduration, artistname
+    mSparqlModel->setQuery(QSparqlQuery(mAlbumsQueryString),*mConnection);
+}
+
+void AlbumsModel::sparqlModelfinished()
+{
+    qDebug() << "underlaying model finished result fetching";
+    emit albumsReady();
+}
+
+
+QHash<int, QByteArray> AlbumsModel::roleNames() const {
+    QHash<int,QByteArray> roles;
+
+    roles[AlbumRole] = "title";
+    roles[SectionRole] = "sectionprop";
+    roles[ArtistRole] = "artist";
+    roles[AlbumCleandRole] = "titleClean";
+    roles[AlbumImageRole] = "coverURL";
+    return roles;
+}
+
+int AlbumsModel::rowCount(const QModelIndex &parent) const {
+    return mSparqlModel->rowCount(parent);
+}
+
+QVariant AlbumsModel::data(const QModelIndex &index, int role) const {
+    switch ( role ) {
+    case AlbumRole:
+        return mSparqlModel->data(index,role);
+        break;
+    case SectionRole:
+        QString albumTitle = mSparqlModel->data(index,0);
+        break;
+    case ArtistRole:
+
+        break;
+    case AlbumCleandRole:
+
+        break;
+    case AlbumImageRole:
+        // FIXME
+        return "";
+        break;
+    default:
+        return "";
+    }
+
+    return "";
 }
--- src/model/albumsmodel.h
+++ src/model/albumsmodel.h
@@ -4,22 +4,38 @@
 #include <QObject>
 #include <Qt5Sparql/QSparqlQueryModel>
 
-class AlbumsModel : public QSparqlQueryModel
+class AlbumsModel : public QAbstractListModel
 {
     Q_OBJECT
+
+    enum EntryRoles {
+        AlbumRole = Qt::UserRole + 1,
+        SectionRole,
+        ArtistRole,
+        AlbumCleandRole,
+        AlbumImageRole
+    };
+
 public:
     explicit AlbumsModel(QObject *parent = 0, QSparqlConnection *connection = 0);
+
+    Q_INVOKABLE QHash<int, QByteArray> roleNames() const;
+    Q_INVOKABLE int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role) const;
 
 signals:
     void albumsReady();
 
 public slots:
     void requestAlbums();
+    void sparqlModelfinished();
 
 
 private:
+    int mRolesCount;
     QString mAlbumsQueryString;
     QSparqlConnection *mConnection;
+    QSparqlQueryModel *mSparqlModel;
 
 };
 
