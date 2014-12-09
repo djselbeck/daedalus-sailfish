e781205e7612bce9f50045ba179a9f4d599a1e29
Hendrik Borghorst <hendrikborghorst@gmail.com>
2014-12-09 08:52:10 +0100

    Empty class for playback state resumeing

---
+++ src/model/playbackstate.cpp
@@ -0,0 +1,6 @@
+#include "playbackstate.h"
+
+PlaybackState::PlaybackState(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ src/model/playbackstate.h
@@ -0,0 +1,18 @@
+#ifndef PLAYBACKSTATE_H
+#define PLAYBACKSTATE_H
+
+#include <QObject>
+
+class PlaybackState : public QObject
+{
+    Q_OBJECT
+public:
+    explicit PlaybackState(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // PLAYBACKSTATE_H
